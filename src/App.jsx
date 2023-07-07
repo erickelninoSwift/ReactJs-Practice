f98594e4e9579af92fae22dfaca85ed51750374e
Erick El nino <erick.tshimbombo@yahoo.com>
2023-07-08 00:11:01 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Student from './test';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my Website</h1>
-    </div>
+      <h1>Welcome to Website</h1>
+      <div>
+        <Student/> 
+      </div>
+    </div>  
   );
 }
-
 export default App;
---
+++ src/Test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+
+function Student()
+{
+    return (
+        <div className="App">
+          <h1> I was not born yesterday ,</h1>
+        </div>
+    );
+}
+
+
+export default Student;
